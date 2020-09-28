7fe37e7bd7dc1c1f3348fb40676b6270b1d16b78
Antonio <antonio.jpsalves@gmail.com>
2020-09-28 10:49:58 -0300

    Solução do ex 8

--- exercicios/condicional/exercicio8.py
+++ exercicios/condicional/exercicio8.py
@@ -2,11 +2,29 @@ n1 = int(input("Digite um número: "))
 n2 = int(input("Digite um número: "))
 n3 = int(input("Digite um número: "))
 
-if n1 > n2 or n1 > n3:
+if n1 > n2 and n1 > n2 and n2 > n3:
     maior = n1
-elif n1 > n2 and n1 < n3:
+    medio = n2
+    menor = n3
+elif n1 > n2 and n1 > n3 and n3 > n2:
+    maior = n1
+    medio = n3
+    menor = n2
+elif n2 > n1 and n2 > n3 and n3 > n1:
+    maior = n2
+    medio = n3
+    menor = n1
+elif n2 > n1 and n2 > n3 and n1 > n3:
+    maior = n2
+    medio = n1
+    menor = n3
+elif n3 > n1 and n3 > n2 and n2 > n1:
+    maior = n3
+    medio = n2
+    menor = n1
+elif n3 > n1 and n3 > n2 and n1 > n2:
     maior = n3
     medio = n1
     menor = n2
-elif n1 < n2 and n1 < n3:
-    menor = n1
+
+print("Em ordem crescente os números informados ficam: %i, %i e %i" %(menor, medio, maior))
